9941140ac87a33295a286486b7cc47137dc8853c
Jennifer Lim <contact@jendigitalvision.com>
2018-02-18 23:34:05 +0100

    US-03: add order confirmation

--- react-ui/src/app/pages/cart/components/confirmation/index.js
+++ react-ui/src/app/pages/cart/components/confirmation/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+
 import MainLayout from '../../../../layouts/default';
+import Pizzas from '../pizzas';
 
 import './index.css';
 
@@ -13,16 +15,14 @@ export default class Ambitions extends Component {
             <MainLayout>
                 <div id='confirmation'>
                     <div className='content'>
-                        <img alt='confirmation' src='' />
-                    
                         <h2>Order Confirmation</h2>
-
                         <h3> Your order will be ready in <span> 30 minutes! </span> </h3>
                         <div id='order'>
                             <h4>
                                 Order Details
                             </h4>
                             <div className='details'>
+                                <Pizzas step='confirmation' />
                             </div>
                         </div>
                     </div>
